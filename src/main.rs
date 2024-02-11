7a089ecb26fc1c6d8ed567dcfe5d59f8a525b924
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2024-02-11 17:14:44 +0200

    fix moving and editing

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
+use home::{self, home_dir};
 use ratatui::{
     layout::Rect,
     prelude::{CrosstermBackend, Terminal},
     style::{Color, Style},
     widgets::Paragraph,
 };
-use std::{error::Error, fs::create_dir_all, io::{stdout, Result, Write}, path::PathBuf};
-use std::io::{BufReader, BufWriter};
+use std::fs::create_dir_all;
 use std::fs::File;
-use std::path::Path;
-use home::{self, home_dir};
+use std::io::{stdout, Result};
+use std::io::{BufReader, BufWriter};
 
+use serde::{Deserialize, Serialize};
+use serde_json::{self, json};
 use TodoType::{Backlog, Done, InProgress};
-use serde_json::{self, json, Value};
-use serde::{Serialize, Deserialize};
 
 enum TodoType {
     Backlog,
@@ -38,11 +38,11 @@ struct Todos {
 impl Todos {
     fn new() -> Todos {
         Todos {
-            tasks: Tasks{
+            tasks: Tasks {
                 backlog: Vec::new(),
                 in_progress: Vec::new(),
                 done: Vec::new(),
-            }
+            },
         }
     }
 
@@ -58,7 +58,7 @@ impl Todos {
             file_path.push("tasks.json");
 
             let tasks_file = File::create(file_path)?;
-            
+
             let writer = BufWriter::new(tasks_file);
             serde_json::to_writer_pretty(writer, &json)?;
         }
@@ -77,17 +77,14 @@ impl Todos {
         Ok(())
     }
 
-    fn add_todo(&mut self, new_item: &str, todo_type: TodoType) {
-        match todo_type {
-            TodoType::Backlog => self.tasks.backlog.push(new_item.to_string()),
-            TodoType::InProgress => self.tasks.in_progress.push(new_item.to_string()),
-            TodoType::Done => self.tasks.done.push(new_item.to_string()),
-        }
+    fn add_task(&mut self, todo_type: &TodoType, index: usize) {
+        self.get_list(&todo_type).insert(index, String::new());
     }
 
-    fn delete_task(&mut self, todo_type: &TodoType, index: usize) {
+    fn delete_task(&mut self, todo_type: &TodoType, index: usize) -> std::io::Result<()> {
         self.get_list(todo_type).remove(index);
-        self.save_to_file();
+        self.save_to_file()?;
+        Ok(())
     }
 
     fn _print_todos(&self) {
@@ -267,12 +264,39 @@ fn input_loop(
                             }
                         }
                         KeyCode::Char('d') => {
-                            todos.delete_task(&chosen_list, current_index);
+                            todos.delete_task(&chosen_list, current_index)?;
                         }
                         _ => (),
                     }
                 } else {
                     // Move cursor
+
+                    if !editing {
+                        match key.code {
+                            KeyCode::Up => {
+                                if current_index > 0 {
+                                    current_index -= 1;
+                                }
+                            }
+                            KeyCode::Down => {
+                                if current_index + 1 < current_list.len() {
+                                    current_index += 1;
+                                }
+                            }
+                            KeyCode::Left => match chosen_list {
+                                InProgress => chosen_list = Backlog,
+                                Done => chosen_list = InProgress,
+                                _ => (),
+                            },
+                            KeyCode::Right => match chosen_list {
+                                InProgress => chosen_list = Done,
+                                Backlog => chosen_list = InProgress,
+                                _ => (),
+                            },
+                            _ => (),
+                        }
+                    }
+
                     match key.code {
                         KeyCode::Char(key_char) => {
                             if editing {
@@ -286,7 +310,7 @@ fn input_loop(
                                         } else {
                                             current_index
                                         };
-                                        current_list.insert(insert_index, String::new());
+                                        todos.add_task(&chosen_list, insert_index);
                                         current_index += 1;
                                         editing = true;
                                     }
@@ -308,26 +332,6 @@ fn input_loop(
                                 todos.save_to_file()?;
                             }
                         }
-                        KeyCode::Up => {
-                            if current_index > 0 {
-                                current_index -= 1;
-                            }
-                        }
-                        KeyCode::Down => {
-                            if current_index + 1 < current_list.len() {
-                                current_index += 1;
-                            }
-                        }
-                        KeyCode::Left => match chosen_list {
-                            InProgress => chosen_list = Backlog,
-                            Done => chosen_list = InProgress,
-                            _ => (),
-                        },
-                        KeyCode::Right => match chosen_list {
-                            InProgress => chosen_list = Done,
-                            Backlog => chosen_list = InProgress,
-                            _ => (),
-                        },
                         _ => (),
                     }
                 }
